00c0a5cb988c57a5502f6afae7f3f8308ddcb489
Sebastian Peterlin <speterlin@gmail.com>
2025-11-20 21:23:48 -0800

    Update pyproject.toml ‘Homepage’ section to reflect correctly named ‘speterlin-stocks’ repo, update __init__.py to reflect most up-to-date description of package and author name and version and ensure that module1 code is imported

--- speterlin-stocks/__init__.py
+++ speterlin-stocks/__init__.py
@@ -1,10 +1,9 @@
-"""An unofficial Python module for a suite of quant-trading opportunities in stocks with API integration:
-        Alpaca brokerage for storing USD assets and margin trading (2x) on exchanges NYSE, NASDAQ
-        Financial Modeling Prep (FMP), Yahoo Finance, Google Finance data collection
-        OpenAI, Gemini-Pro AI analysis
+"""A Python module for a suite of quant-trading opportunities in stocks with API integration: Alpaca brokerage for storing USD assets and margin trading (2x) on exchanges NYSE & NASDAQ, Financial Modeling Prep (FMP) & Yahoo Finance & Google Finance & ExchangeRate & MarketBeat & CrunchBase for data collection, OpenAI & Google Gemini-Pro for AI analysis
 
-.. moduleauthor:: Sebastian Peterlin
+.. moduleauthor:: Sebastian B. Peterlin
 
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.2'
+
+from .module1 import .
